c6217da13dd5f77d1cc4e29f9f5008511c4dd3ab
Roger <luojie.5408@163.com>
2018-07-10 17:33:54 +0800

    11

--- index.js
+++ index.js
@@ -7,13 +7,13 @@ const app = {
     _store: null,
     _constants: {},
     _onStart: [],
+    _config: { effects: {} }, //初始值，后面会用到
     onStart,
     start
 } 
-const config = { effects: {} } //初始值，后面会用到
 const sagaMiddleware = createSagaMiddleware()
 app._store = createStore(a => a, applyMiddleware(sagaMiddleware))
 
-export const Model = getModel(app._store,config,sagaMiddleware)
+export const Model = getModel(app._store,app._config,sagaMiddleware)
 export { connect }
 export default app
--- start.js
+++ start.js
@@ -2,13 +2,13 @@ import invariant from 'invariant'
 import React from 'react'
 import { Provider } from 'react-redux'
 let alreadyStarted = false
-function start(Component,_conf={}){
+function start(Component,config={}){
     invariant(Component,'dvax.start第一个参数不能为空，需传入react组件')
     invariant(!alreadyStarted,'dvax已经初始化过一次了')
     alreadyStarted = true
-    invariant(typeof(_conf) ==='object','config文件应该为object')
-    Object.keys(_conf).forEach(key=>{
-        config[key] = _conf[key]               
+    invariant(typeof(config) ==='object','config文件应该为object')
+    Object.keys(config).forEach(key=>{
+        this._config[key] = config[key]               
     })
     this._onStart.forEach(cb=>{
         cb && cb()

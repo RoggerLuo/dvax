7f078ae7a0ffb7c1697c91dbc5a4ab68210a2868
Roger <luojie.5408@163.com>
2018-07-19 15:54:37 +0800

    update

--- Model/sagaParams.js
+++ Model/sagaParams.js
@@ -2,7 +2,7 @@ import { put, call } from 'redux-saga/effects'
 import invariant from 'invariant'
 
 export default (namespace) => {
-    return { put: prefixedPut, dispatch: prefixedPut, change, reduce, call }   
+    return { put: prefixedPut, dispatch: prefixedPut, change, reduce, call, get }   
     function prefixedPut(action) {
         action.type = `${namespace}/${action.type}`
         return put(action)
@@ -16,4 +16,9 @@ export default (namespace) => {
         invariant(reducer,'reduce方法需要传入reducer')
         return put({ type: `${namespace}/std`, reducer })
     }
+    function get(_namespace){
+        if(!_namespace) return store.getState()[namespace]
+        return store.getState()[_namespace]
+    }
+
 }

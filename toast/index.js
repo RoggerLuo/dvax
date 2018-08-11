607e7513095c87385f60c96c7db4c26ef78dec7c
Roger <luojie.5408@163.com>
2018-08-11 13:10:20 +0800

    patch

--- toast/index.js
+++ toast/index.js
@@ -1,23 +1,28 @@
 import invariant from 'invariant'
 import React from 'react'
 import { Model } from '../index.js'
+let showing = false
 Model.create({
     namespace:'dvaxToast',
     state:{ status:false, message:'', show:false },
     reducers:{
         show(state,{ status, message }){
-            return { ...state, show:true, message, status}
+            return { ...state, show: true, message, status}
         },
         hide(state){
             return { ...state, show:false}
         },
         clear(state){
+            showing = false
             return { ...state, message:'', status:false}
         }
     }
 })
 
 export default function(message,duration,status){
+    if(showing) return
+    showing = true
+
     if(Model.get('dvaxToast').show) return
     invariant(message,'提示信息不能为空[from dvax/toast]')
     invariant(duration,'持续时间不能为空[from dvax/toast]')

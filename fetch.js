aa4933b70a6cc6165f78d745488b4ce7a41019f7
Roger <luojie.5408@163.com>
2018-11-22 18:12:49 +0800

    fetch 优化

--- fetch.js
+++ fetch.js
@@ -1,86 +1,66 @@
 import invariant from 'invariant'
 export default function(config) {
+    if(typeof(config) === 'function') {
+        config = config()
+    }
     return (url, options) => {
+        // options
         if(!options) options = {}
         options = { ...options }
-
-        if(typeof(config) == 'function') {
-            config = config()
-        }
-        
+        if(!options.headers){
+            options.headers = {}  
+        } 
+        if (!options.method) options.method = "GET"
+        options.method = options.method.toUpperCase()
+        // config        
         let { baseUrl, headers, bodyTransform } = config
-
         if(!baseUrl) baseUrl = ''
-        // initial
-        options.headers = {}
-        // options.credentials = 'include'
-
-        // url things
         if(baseUrl[baseUrl.length-1] == '/') {
             url = baseUrl + url
         }else{
             url = baseUrl + '/' + url            
         }
-
-        // method things
-        if (!options.method) options.method = "GET"
-        options.method = options.method.toUpperCase()
+        // apply config to options
         if (options.method === 'POST' || options.method === 'PUT') {
             if(bodyTransform){
                 options.body = bodyTransform(options.body)
             }  
         }
-
+        if (headers) {
+            options.headers = { ...options.headers, ...headers }
+        }
+        // support query
         const configQuery = config.query || {}
         const optionsQuery = options.query || {}
         const finalQuery = { ...configQuery, ...optionsQuery }
         if (JSON.stringify(finalQuery) !== "{}") {
-            url = url + transformQuery(finalQuery)
+            url = url + transformQuery(finalQuery) 
         }
 
-
-        if (headers) {
-            options.headers = { ...options.headers, ...headers }
-        }
-        let parser = a => a
-        if(config.parser) parser = config.parser
-        if(options.parser) parser = options.parser
         return fetch(url, options)
-            .then(checkStatus)
-            .then(parseText)
-            .then(parseJSON)
-            .then(res=>{
-                return parser(res)
-            })
-            .catch(err => {
-                return parser({ error:true, message: err.message })
-            })
+            .then(config.checkStatus || checkStatus)
+            .then(config.parseText || parseText)
+            .then(config.parseJSON || parseJSON)
+            .then(config.receiveData || receiveData)
+            .catch(err => receiveData({ hasError:true, error: err }))
     }
+}
+function receiveData(res){
+    return res
 }
 function parseJSON(text){ // if it can
     try{
         return JSON.parse(text)
     }catch(err){
-        console.log('尝试进行JSON.parse失败:',err.message,'返回原始值')
         return text
     }
 }
 function parseText(response) {
     return response.text()
 }
-
 function checkStatus(response) {
-    if (response.status >= 200 && response.status < 300) {
-        return response
-    }else{
-        return { 
-            text(){
-                return { error: true, type: response.status, message: response.statusText }
-            }
-        }
-    }
+    return response
 }
-
 function transformQuery(query) {
     let queryStr = '?'
     for (let k in query) {

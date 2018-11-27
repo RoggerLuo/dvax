9928aa29c5eec9fa979567d7c43841e847037dd2
Roger <luojie.5408@163.com>
2018-11-27 14:21:22 +0800

    带cookies

--- fetch.js
+++ fetch.js
@@ -12,6 +12,7 @@ export default function(config) {
         } 
         if (!options.method) options.method = "GET"
         options.method = options.method.toUpperCase()
+        options.credentials = 'include'
         // config        
         let { baseUrl, headers, requestBody } = config
         if(!baseUrl) baseUrl = ''
